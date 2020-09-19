d3afcb424b3338186f8f301002330ebaa0392a04
hg1010 <ghlrms1012@naver.com>
2020-09-19 22:01:29 +0900

    init_test

---
+++ hello.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+if __name__=='__main__':
+	try:
+		rospy.init_node('hello_ros')
+		p = rospy.Publisher('hello', String, queue_size=1)
+		r = rospy.Rate(1)
+		cnt = 0
+
+		while not rospy.is_shutdown():
+			cnt=cnt+1
+			msg = 'hello ~%s' %cnt
+			print(msg)
+			p.publish(msg)
+			r.sleep()
+
+	except rospy.ROSInterruptException:
+		pass
+
+
